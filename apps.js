9f0496e7db091e87a7e62c4f8493f00e8b0576e9
Md Zubayer Al Hasan <jubayeralh@gmail.com>
2023-07-15 21:01:16 +0600

    initial commit

---
+++ apps.js
@@ -0,0 +1,12 @@
+const hamberger = document.querySelector(".hamberger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamberger.addEventListener( "click", () =>{
+    hamberger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+})
+document.querySelectorAll(".menu-link").forEach ( n => n.addEventListener("click", () => {
+    hamberger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
